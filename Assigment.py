b89ee2b9f73fdbc7dae5b2ead586ac89204d291d
Mohammad Arshadulla Noor <53905420+Arshadulla1@users.noreply.github.com>
2023-03-16 00:51:44 +0530

    Add files via upload

---
+++ Assigment.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+# 
+
+# <b>Mohammad Arshadulla Noor</b>
+
+# Student
+
+# *Im Interested in data Science beacuse I enjoy being a part of new technologies and trying out innovative solutions*
+
+# ## The following code is for addition
+
+# In[4]:
+
+
+n=2
+def add(n):
+    return (n+n)
+print(n)
+
+
+# - one
+# - two
+# - three
+# - four
+# - five
+# - six
+# 
+
+# | Name | Age | Gender |
+# | --- | --- | --- |
+# | Sam | 22 | Male |
+
+# [Coursera](https://www.coursera.com)
