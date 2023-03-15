5aa7fa1c14d584c59b54ea64956e9f3693a4bfd7
Mohammad Arshadulla Noor <53905420+Arshadulla1@users.noreply.github.com>
2023-03-16 01:33:11 +0530

    Add files via upload

---
+++ Coursera Assignment.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook
+# 
+
+# <b> I am Mohammad Arshadulla Noor</b>
+# Im Interested in data Science beacuse I enjoy being a part of new technologies and trying out innovative solutions
+
+# ## Data science Languages
+# - Python
+# - R
+# - Scala
+# - Java
+# - Julia
+# - Go
+
+# ## Data Science Libraries
+# - scikit-learn
+# - TensorFlow
+# - Pandas
+# - PyTorch
+
+# ## Data Science Tools
+# |Tool Name | Used for |
+# | --- | --- |
+# | Apache Spark | Processing Data |
+# | MATLAB | simulating neural networks |
+# | Excel | Data Analysis |
+
+# ## Introducing arithmetic expression examples
+
+# ### Multiply
+
+# In[12]:
+
+
+n=2
+m=2
+print(n*m)
+
+
+# ### Add
+
+# In[11]:
+
+
+#Add
+a=4
+b=5
+print(a+b)
+
+
+# ### Convert minutes to hours
+
+# In[25]:
+
+
+mins = 75
+hours = mins / 60
+print(str(hours) + " hours")
+
+
+# - Data Science languages
+# - Data Science libraries
+# - Data Science Tools
+# - Arithmetic Expressions
+# - Minutes to hours code
+
+# "title": "My Notebook",
+# "author": [
+# {"name": "Mohammad Arshadulla Noor"}
+# ]
